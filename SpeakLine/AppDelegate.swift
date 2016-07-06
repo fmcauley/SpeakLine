7abe9d61b82a905cd81b3c972a5fdc29a71292a7
Discovery Dev <adsdiscoveryteam@gmail.com>
2016-07-05 21:38:22 -0400

    Initial Commit

---
+++ SpeakLine/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SpeakLine
+//
+//  Created by MLS Discovery on 7/5/16.
+//  Copyright © 2016 SoftwareSoFast. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
