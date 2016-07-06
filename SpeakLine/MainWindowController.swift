36fd707458e48e09111ddecc932242b945221d0d
Discovery Dev <adsdiscoveryteam@gmail.com>
2016-07-05 22:05:56 -0400

    Added the controls, the outlets, and the actions. Also hooked all the elements up.

--- SpeakLine/MainWindowController.swift
+++ SpeakLine/MainWindowController.swift
@@ -12,10 +12,29 @@ class MainWindowController: NSWindowController {
     override var windowNibName: String {
         return "MainWindowController"
     }
+    
+    @IBOutlet weak var textField: NSTextField!
+    @IBOutlet weak var speakButton: NSButton!
+    @IBOutlet weak var stopButton: NSButton!
 
     override func windowDidLoad() {
         super.windowDidLoad()
 
+    }
+    
+    // Action Methods
+    @IBAction func speakIt(sender: NSButton) {
+        // Get typed-in text as a string
+        let string = textField.stringValue
+        if string.isEmpty {
+            print("stirng from \(textField!) is empty")
+        } else {
+            print("string is \"\(textField.stringValue)\"")
+        }
+    }
+    
+    @IBAction func stopIt(sender: NSButton) {
+        print("stop button clicked")
     }
     
 }
