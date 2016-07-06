701f72011a15e49ea198c163f6e75a84ea60d719
Discovery Dev <adsdiscoveryteam@gmail.com>
2016-07-06 19:00:16 -0400

    Adding a delegate call for the NSWindowDelegate

--- SpeakLine/MainWindowController.swift
+++ SpeakLine/MainWindowController.swift
@@ -8,7 +8,7 @@
 
 import Cocoa
 
-class MainWindowController: NSWindowController, NSSpeechSynthesizerDelegate {
+class MainWindowController: NSWindowController, NSSpeechSynthesizerDelegate, NSWindowDelegate {
     override var windowNibName: String {
         return "MainWindowController"
     }
@@ -67,11 +67,20 @@ class MainWindowController: NSWindowController, NSSpeechSynthesizerDelegate {
         self.isStarted = false
     }
     
+    // MARK: - NSWindowDelegate
+    func windowShouldClose(_ sender: AnyObject) -> Bool {
+        return isStarted
+    }
+    
     /**Being a delegate 
      
      The first thing to understand about being a delegate is that it is a role – possibly one of many – that an object may take on. In Cocoa, roles are defined by protocols. The specific role of “delegate that serves a speech synthesizer” is defined by the NSSpeechSynthesizerDelegate protocol. To sign up for this role, an object must be an instance of a class that conforms to the NSSpeechSynthesizerDelegate protocol. 
      
     The beauty of having delegate roles defined by protocols (delegation) instead of classes (subclassing) is that any object whose type conforms to the protocol can serve in that capacity. Thus, you can assign critical duties without associating them with any particular object class. Moreover, an object can take on multiple roles as needed.
+     
+     This is an example of a second type of delegate method – one where the delegate can directly affect the behavior of the object as opposed to being passively informed of events.
+     
+     Some delegate proerties can be set in code or in the XIB. the NSWindowDelegate is already set for us in the XIB file
      
      */
     
