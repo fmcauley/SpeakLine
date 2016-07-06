fa23337d2e2e374da3bc180f92311a738e0b8a2c
Discovery Dev <adsdiscoveryteam@gmail.com>
2016-07-05 21:50:58 -0400

    The initial commit.

---
+++ SpeakLine/MainWindowController.swift
@@ -0,0 +1,21 @@
+//
+//  MainWindowController.swift
+//  SpeakLine
+//
+//  Created by MLS Discovery on 7/5/16.
+//  Copyright © 2016 SoftwareSoFast. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    override var windowNibName: String {
+        return "MainWindowController"
+    }
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+    }
+    
+}
